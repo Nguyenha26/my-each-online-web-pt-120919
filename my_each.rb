cd3de0ff3bf7327f0a4a4c0ee58046644fbeb90f
Nguyen Ha <nynha0526@gmail.com>
2019-12-22 01:38:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,1 @@
-list = ["ab", "cd", "ef"]
-
-def my_each(array. index)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
-end
-
-my_each(list) {|i| puts "So I think #{i} is a piece of cake"}
 
